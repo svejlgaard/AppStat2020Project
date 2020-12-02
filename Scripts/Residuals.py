b39b25bf8b64af625cc365b327733f0d3d085926
svejlgaard <fgj787@alumni.ku.dk>
2020-12-02 11:04:30 +0100

    Changing file to include function

--- Scripts/Residuals.py
+++ Scripts/Residuals.py
@@ -14,49 +14,68 @@ random_state = 27
 plt.style.use('seaborn')
 
 
-filename = '../Data/Pendulum/'
+def get_period(person_name):
 
-filename += input('Please insert the name of the data file: ')
+    filename = '../Data/Pendulum/'
 
-times = np.loadtxt(filename)
+    filename += f'{person_name}Timing.dat'
 
-
-print('WARNING: Always removing the first time in the data file')
-
-times = times[1:]
-
-x = times[:,0]
-
-y = times[:,1]
-
-fit_values = np.polyfit(x,y,1, full=True)
-
-fit_param = fit_values[0]
-fit_slope = fit_param[0]
-fit_cutoff = fit_param[1]
-
-fit_residuals = y - (x*fit_slope + fit_cutoff)
-
-residual_err = np.std(fit_residuals)
+    times = np.loadtxt(filename)
 
 
-fig, ax = plt.subplots(nrows=2, ncols=1, figsize=(8, 10),
+    print('WARNING: Always removing the first time in the data file')
+
+    times = times[1:]
+
+    x = times[:,0]
+
+    y = times[:,1]
+
+    fit_values = np.polyfit(x,y,1, full=True)
+
+    fit_param = fit_values[0]
+    fit_slope = fit_param[0]
+    fit_cutoff = fit_param[1]
+
+    fit_residuals = y - (x*fit_slope + fit_cutoff)
+
+    residual_err = np.std(fit_residuals)
+    y_err = np.ones_like(y) * residual_err
+
+    fig, ax = plt.subplots(nrows=2, ncols=1, figsize=(8, 10),
                        gridspec_kw={'height_ratios':[4,1]}, sharex=True)
-ax[0].errorbar(x,y, yerr=residual_err,fmt='.', label=f'Data with errors from fit {residual_err:.2f} s')
-ax[0].plot(x, x*fit_slope+fit_cutoff,label='Fit')
+    ax[0].errorbar(x,y, yerr=residual_err,fmt='.', label=f'Data with error +- {residual_err:.2f} (s) from fit')
+    ax[0].plot(x, x*fit_slope+fit_cutoff,label=f'Fit')
 
-ax[1].plot(x, fit_residuals, label='Residuals')
+    ax[0].set_ylabel('Time elapsed (s)')
 
-fig.legend(loc='best')
+    ax[1].plot(x, fit_residuals, label='Residuals')
 
-name = f'{filename.replace(".dat","")}_plot_with_residuals.pdf'
+    fig.legend()
 
-name = name.split('/')[-1]
+    name = f'{filename.replace(".dat","")}_plot_with_residuals.pdf'
 
-full_name = '../Figures/Pendulum/'
+    name = name.split('/')[-1]
 
-full_name += name
+    full_name = '../Figures/Pendulum/'
 
-fig.savefig(full_name)
+    full_name += name
 
-print(f'The figure has been saved as {full_name}')
+    fig.savefig(full_name)
+
+    print(f'The figure has been saved in {full_name.replace("..","")}')
+
+
+    from scipy.optimize import curve_fit
+  
+    def f(x, a, b):
+        return a*x + b
+    
+    popt, pcov = curve_fit(f,x,y,sigma=y_err)
+    perr = np.sqrt(np.diag(pcov))
+    
+
+    return popt[0], perr[0]
+
+
+get_period('Simone')
