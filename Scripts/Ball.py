5fa7a7b6ba443703cddeb3eac24b8f110e97c107
svejlgaard <fgj787@alumni.ku.dk>
2020-12-06 17:09:38 +0100

    Implementing the other group's naming sheme

--- Scripts/Ball.py
+++ Scripts/Ball.py
@@ -22,27 +22,32 @@ class Ball():
     def __init__(self, name):
         self.name = name
         self.diameter = get_diameters()[self.name]
+        if self.name == 'SB1':
+            self.ball = 'mid'
+        if self.name == 'LB1':
+            self.ball = 'big'
     
-    def get_peaks(self,no_jump = True, flipped = True):
+    def get_peaks(self, orientation):
+
         import glob
-        filename = f'../Data/BallIncline/{self.name}'
-        if no_jump and flipped:
-            filename += '_flipped_nojump.csv' 
-        if no_jump and not flipped:
-            filename += '_nojump.csv'
-        if not no_jump and flipped:
-            filename += '_flipped.csv'
-        if not no_jump and not flipped:
-            filename += '.csv'
-        filename = '../Data/ball7_8mm_example.csv'
-        dat = np.genfromtxt(filename, delimiter=',', skip_header=13, names=True)
-        time = dat['Time_s']
-        voltage = dat['Channel_1_V']
-        peaks, _ = find_peaks(voltage, height=4, distance=100)
-        assert len(peaks) == 5
-        t = time[peaks]
-        self.times = t
-        return t
+        self.orientation = orientation
+        files = glob.glob(f'../Data/BallIncline/*{self.ball}_{self.orientation}.csv')
+        
+        time_list = list()
+        for f in files:
+            dat = np.genfromtxt(f, delimiter=',', skip_header=13, names=True)
+            time = dat['Time_s']
+            voltage = dat['Channel_1_V']
+            peaks, _ = find_peaks(voltage, height=4, distance=100)
+            assert len(peaks) == 5
+            t = time[peaks]
+            time_list.append(t)
+        time_list = np.array(time_list)
+        time_list = time_list - time_list[:,0,None]
+        self.times = np.mean(time_list,axis=0)
+        self.times_err = np.std(time_list, axis=0) / np.sqrt(3)
+        
+        return self.times
 
     def get_diameter(self):
         self.diameter_mean = self.diameter.mean()
@@ -76,13 +81,13 @@ class Ball():
         position_fit = Minuit(acc_fit, pedantic=False, a=2.0, b=0.0, c=self.length_mu[0])
         position_fit.migrad()
 
-        plt.figure('Position')
-        plt.errorbar(self.times, self.length_mu, yerr=self.length_mu_err, fmt='.', label='Data')
+        plt.figure(f'Position_{self.ball}_{self.orientation}')
+        plt.errorbar(self.times, self.length_mu, yerr=self.length_mu_err, xerr=self.times_err, fmt='.', label='Data')
         plt.plot(self.times, s(self.times , *position_fit.args), label=f'Fit with a = {position_fit.values["a"]:.2f} +- {position_fit.errors["a"]:.2f}')
         plt.xlabel('Time (s)')
         plt.ylabel('Position (m)')
         plt.legend()
-        plt.savefig('../Figures/BallIncline/Acceleration_plot.pdf')
+        plt.savefig(f'../Figures/BallIncline/Acceleration_plot_{self.ball}_{self.orientation}.pdf')
 
         self.a = position_fit.values["a"]
         self.a_err = position_fit.errors["a"]
@@ -90,22 +95,23 @@ class Ball():
 
     def get_angle(self):
         angles = get_angles()
-        phi_r = angles[['inc_r', 'inc_flip_r']]
-        phi_l = angles[['inc_l','inc_flip_l']]
 
-        phi_r = phi_r.mean(axis=0)
-        phi_l = phi_l.mean(axis=0)
+        phi_r = angles[['inc_r']]
+        phi_l = angles[['inc_l']]
 
-        self.phi_r_mu = phi_r.mean()
-        self.phi_r_std = phi_r.std()
+        phi_r = np.radians(phi_r)
+        phi_l = np.radians(phi_l)
+
+        self.phi_r_mu = phi_r.values.mean()
+        self.phi_r_std = phi_r.values.std()
         self.phi_r_mu_err = self.phi_r_std / np.sqrt(3)
 
-        self.phi_l_mu = phi_l.mean()
-        self.phi_l_std = phi_l.std()
+        self.phi_l_mu = phi_l.values.mean()
+        self.phi_l_std = phi_l.values.std()
         self.phi_l_mu_err = self.phi_l_std / np.sqrt(3)
 
         self.dphi_mu = self.phi_r_mu - self.phi_l_mu
-        self.dphi_std = self.phi_r_std + self.phi_l_std
+        self.dphi_std = np.sqrt(self.phi_r_std**2 + self.phi_l_std**2)
         self.dphi_mu_err = self.dphi_std / np.sqrt(2)
 
     def get_g(self):
@@ -115,14 +121,17 @@ class Ball():
         self.rail_std = np.std(rail)
         self.rail_mu_err = self.rail_std / np.sqrt(3)
         paranthesis = 1 + ( (2/5) * (self.diameter_mean**2) / (self.diameter_mean**2 - self.rail_mu**2) )
+        if self.orientation == 'L':
+            angle = self.phi_l_mu + self.dphi_mu
+        if self.orientation == 'R':
+            angle = self.phi_r_mu - self.dphi_mu
+        self.g = paranthesis * self.a / np.sin(angle)
         
-        self.g = paranthesis * self.a / np.sin(self.phi_r_mu - self.dphi_mu)
-        
-        err_term1 = paranthesis * 1/np.sin(self.phi_r_mu - self.dphi_mu) * self.a_err**2
-        err_term2 = self.a**2 * paranthesis * np.cos(self.phi_r_mu - self.dphi_mu)**2 / np.sin(self.phi_r_mu - self.dphi_mu)**4 * self.phi_r_std
-        err_term3 = self.a**2 * paranthesis * np.cos(self.phi_r_mu - self.dphi_mu)**2 / np.sin(self.phi_r_mu - self.dphi_mu)**4 * self.dphi_std
-        err_term4 = self.a**2 / np.sin(self.phi_r_mu - self.dphi_mu)**2 * (4*self.diameter_mean / (5* (self.diameter_mean**2 + self.rail_mu**2)) - 4*self.diameter_mean**3 / (5* (self.diameter_mean**2 + self.rail_mu**2)))**2 * self.diameter_std**2
-        err_term5 = self.a**2 * 16 * self.diameter_mean**4 * self.rail_mu**2 / (25 * np.sin(self.phi_r_mu - self.dphi_mu)**2 * (self.diameter_mean**2 - self.rail_mu**2)**4 ) * self.rail_std**2 
+        err_term1 = paranthesis * 1/np.sin(angle) * self.a_err**2
+        err_term2 = self.a**2 * paranthesis * np.cos(angle)**2 / np.sin(angle)**4 * self.phi_r_std
+        err_term3 = self.a**2 * paranthesis * np.cos(angle)**2 / np.sin(angle)**4 * self.dphi_std
+        err_term4 = self.a**2 / np.sin(angle)**2 * (4*self.diameter_mean / (5* (self.diameter_mean**2 + self.rail_mu**2)) - 4*self.diameter_mean**3 / (5* (self.diameter_mean**2 + self.rail_mu**2)))**2 * self.diameter_std**2
+        err_term5 = self.a**2 * 16 * self.diameter_mean**4 * self.rail_mu**2 / (25 * np.sin(angle)**2 * (self.diameter_mean**2 - self.rail_mu**2)**4 ) * self.rail_std**2 
 
         self.g_err = np.sqrt(err_term1 + err_term2 + err_term3 + err_term4 + err_term5)
 
@@ -135,16 +144,20 @@ class Ball():
 
 # Usage example
 
-small_ball = Ball('SB1')
 
-small_ball.get_diameter()
+for size in ['SB1','LB1']:
+    small_ball = Ball(size)
 
-small_ball.get_peaks()
+    small_ball.get_diameter()
 
-small_ball.get_acc()
+    for o in ['R', 'L']:
 
-small_ball.get_angle()
+        small_ball.get_peaks(o)
 
-small_ball.get_g()
+        small_ball.get_acc()
 
-print(f'We find g = {small_ball.g:.2f} +- {small_ball.g_err:.2f} m/s')
+        small_ball.get_angle()
+
+        small_ball.get_g()
+
+        print(f'We find g = {small_ball.g:.2f} +- {small_ball.g_err:.2f} m/s for {small_ball.ball} ball oriented {small_ball.orientation}')
