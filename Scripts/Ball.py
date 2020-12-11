2001cde55b6f2f05ea1bb622e57e2170f3ca4867
svejlgaard <fgj787@alumni.ku.dk>
2020-12-11 10:30:20 +0100

    Boss level comments

--- Scripts/Ball.py
+++ Scripts/Ball.py
@@ -21,8 +21,13 @@ plt.style.use('seaborn')
 
 class Ball():
     def __init__(self, name):
+        # The name should be either 'SB1' (small ball) or 'LB1' (large ball)
         self.name = name
+
+        # Importing the diameter of the given ball
         self.diameter = get_diameters()[self.name]
+
+        # Converting to the naming sheme of the other group
         if self.name == 'SB1':
             self.ball = 'mid'
         if self.name == 'LB1':
@@ -31,20 +36,28 @@ class Ball():
     def get_peaks(self, orientation):
 
         import glob
+        # Orientation should be 'L' (left) or 'R' (right)
         self.orientation = orientation
         files = glob.glob(f'../Data/BallIncline/*{self.ball}_{self.orientation}.csv')
         
         time_list = list()
         for f in files:
+            # Load the data from the relevant file
             dat = np.genfromtxt(f, delimiter=',', skip_header=13, names=True)
             time = dat['Time_s']
             voltage = dat['Channel_1_V']
+            # Using the scipy find_peaks with a minimum height of 4 to avoid detecting noise as peaks
+            # and a minimum distance of 100 to avoid detecting the same peaks twice
             peaks, _ = find_peaks(voltage, height=4, distance=100)
             assert len(peaks) == 5
             t = time[peaks]
             time_list.append(t)
         time_list = np.array(time_list)
+
+        # To 'calibrate' the detections to go though 0 and avoid different cutoffs 
         time_list = time_list - time_list[:,0,None]
+
+        # Saving and calculating error on mean
         self.times = np.mean(time_list,axis=0)
         self.times_err = np.std(time_list, axis=0) / np.sqrt(3)
         
@@ -60,28 +73,41 @@ class Ball():
     def get_acc(self):
         lengths = pd.read_csv('../Data/BallIncline/rail_lengths.txt',sep=',', header=3)
 
+
+        # To get the measures from the folding ruler, I use the index :5
         fold_measure = lengths.iloc[:5,:]
+
+        # The pandas dataframe read_csv adds extra columns without any numbers in them. 
+        # This removes those columns
         fold_measure = fold_measure.reset_index(drop=True)
 
+        # Similar for the ruler, but it has index from line 5 and up (5:)
         ruler_measure = lengths.iloc[5:,:]
         ruler_measure = ruler_measure.reset_index(drop=True)
         
+        # Combining them under the assumption that both observers and equipment are independent
         length_measure = pd.concat([fold_measure, ruler_measure], axis=1, copy=True)
+
+        # Converting from cm to m
         length_measure = length_measure / 100
         self.length_mu = length_measure.mean(axis=1).to_numpy()
         self.length_std = length_measure.std(axis=1).to_numpy()
+
+        # The error on the mean, where the np.sqrt(6) is from the independence assumption
         self.length_mu_err = self.length_std/ np.sqrt(6)
         
-        
+        # Assuming uniform acceleration
         def s(t, a, b, c):
             return 1/2*a*t*t + b*t + c
 
 
-        # Trying to use iMinuit...
+        # Fitting via chi2
         acc_fit = Chi2Regression(f=s, x=self.times, y=self.length_mu, error=self.length_mu_err)
         position_fit = Minuit(acc_fit, pedantic=False, a=2.0, b=0.0, c=self.length_mu[0])
         position_fit.migrad()
 
+
+        # Saving the result as a figure
         plt.figure(f'Position_{self.ball}_{self.orientation}')
         plt.errorbar(self.times, self.length_mu, yerr=self.length_mu_err, xerr=self.times_err, fmt='.', label='Data')
         plt.plot(self.times, s(self.times , *position_fit.args), label=f'Fit with a = {position_fit.values["a"]:.2f} +- {position_fit.errors["a"]:.2f}')
@@ -90,6 +116,8 @@ class Ball():
         plt.legend()
         plt.savefig(f'../Figures/BallIncline/Acceleration_plot_{self.ball}_{self.orientation}.pdf')
 
+
+        # Saving the result as a pickle file as requested by Emma
         data_final = open(f'../Data/BallIncline/Acceleration_plot_{self.ball}_{self.orientation}_results.pkl','wb')
 
         dict_final = {'x': self.times,
@@ -143,10 +171,15 @@ class Ball():
 
     def get_g(self):
         rail = np.loadtxt('../Data/BallIncline/Rail_diameter.txt')
+
+        # Converting from mm to m
         rail = rail / 1000
+
         self.rail_mu = np.mean(rail)
         self.rail_std = np.std(rail)
         self.rail_mu_err = self.rail_std / np.sqrt(3)
+
+        # Based on the equations from the report for the ball 
         paranthesis = 1 + ( (2/5) * (self.diameter_mean**2) / (self.diameter_mean**2 - self.rail_mu**2) )
         if self.orientation == 'L':
             angle = self.phi_l_mu + self.dphi_mu
@@ -154,6 +187,7 @@ class Ball():
             angle = self.phi_r_mu - self.dphi_mu
         self.g = paranthesis * self.a / np.sin(angle)
         
+        # Splitted to 
         err_term1 = paranthesis * 1/np.sin(angle) * self.a_err**2
         err_term2 = self.a**2 * paranthesis * np.cos(angle)**2 / np.sin(angle)**4 * self.phi_r_std
         err_term3 = self.a**2 * paranthesis * np.cos(angle)**2 / np.sin(angle)**4 * self.dphi_std
